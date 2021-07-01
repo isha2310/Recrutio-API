1c92cbf568549c23a1ecba854818d6acd1bd4f7c
Isha Agarwal <isha.a1023@gmail.com>
2021-07-01 23:12:43 +0530

    socket.io added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server,{
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://recrutio.netlify.app",
     },
 });
 
